74926222f8f8567b68d64a970ab2289921c16230
ogrebatel <ogrebatelb@gmail.com>
2022-10-21 22:58:32 +0300

    workflow with container

--- linters/linters.sh
+++ linters/linters.sh
@@ -25,7 +25,7 @@ print_header "RUN clang-tidy"
 check_log "clang-tidy ./Tests/*.cpp -warnings-as-errors=* -extra-arg=-std=c++17 -- -Iproject/include" "Error (?:reading|while processing)"
 
 print_header "RUN cppcheck"
-check_log "cppcheck project --enable=all --inconclusive --error-exitcode=1 --suppress=missingIncludeSystem" "\(information\)"
+check_log "cppcheck ./Tests/*.cpp --enable=all --inconclusive --error-exitcode=1 --suppress=missingIncludeSystem" "\(information\)"
 
 print_header "RUN cpplint"
 check_log "cpplint ./Tests/*.cpp" "Can't open for reading"
