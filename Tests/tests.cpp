49f62b0e63d657dfd7620d88c4c7da340d5eaa66
moraPresence <od34156la@mail.ru>
2022-11-10 15:41:05 +0300

    some fix after first review

--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -2,7 +2,7 @@
 
 #include <gtest/gtest.h>
 
-TEST(Test, test_for_test){
+TEST(Test, test_for_test) {
     EXPECT_TRUE(true);
 }
 
--- linters/linters.sh
+++ linters/linters.sh
@@ -3,31 +3,34 @@
 set -o pipefail
 
 function print_header() {
-    echo -e "\n***** ${1} *****"
+  echo -e "\n***** ${1} *****"
 }
 
 function check_log() {
-    LOG=$( { ${1}; } 2>&1 )
-    STATUS=$?
-    echo "$LOG"
-    if echo "$LOG" | grep -q -E "${2}"
-    then
-        exit 1
-    fi
+  LOG=$({ ${1}; } 2>&1)
+  STATUS=$?
+  echo "$LOG"
+  if echo "$LOG" | grep -q -E "${2}"; then
+    exit 1
+  fi
 
-    if [ $STATUS -ne 0 ]
-    then
-        exit $STATUS
-    fi
+  if [ $STATUS -ne 0 ]; then
+    exit $STATUS
+  fi
 }
 
-print_header "RUN clang-tidy"
-check_log "clang-tidy ./Tests/*.cpp -warnings-as-errors=* -extra-arg=-std=c++17 -- -Iproject/include" "Error (?:reading|while processing)"
+if ${1} -eq "--clang" || ${1} -eq "--all"; then
+  print_header "RUN clang-tidy"
+  check_log "clang-tidy ./Tests/*.cpp -warnings-as-errors=* -extra-arg=-std=c++17 -- -Iproject/include" "Error (?:reading|while processing)"
+fi
 
-print_header "RUN cpplint"
-check_log "cpplint ./Tests/*.cpp" "Can't open for reading"
+if ${1} -eq "--cpplint" || ${1} -eq "--all"; then
+  print_header "RUN cpplint"
+  check_log "cpplint ./Tests/*.cpp" "Can't open for reading"
+fi
 
-print_header "RUN cppcheck"
-check_log "cppcheck ./Tests/*.cpp --enable=all --inconclusive --error-exitcode=1 --suppress=missingIncludeSystem" "\(information\)"
-
+if ${1} -eq "--cppcheck" || ${1} -eq "--all"; then
+  print_header "RUN cppcheck"
+  check_log "cppcheck ./Tests/*.cpp --enable=all --inconclusive --error-exitcode=1 --suppress=missingIncludeSystem" "\(information\)"
+fi
 print_header "SUCCESS"
