a8bae23dfb58dad1eb73108ea00b475c3987a8e6
moraPresence <od34156la@mail.ru>
2022-11-11 20:36:08 +0300

    to review

--- scripts/linters/linters.sh
+++ scripts/linters/linters.sh
@@ -31,6 +31,6 @@ fi
 
 if [ "$1" = "--cppcheck" ] || [ "$1" = "--all" ]; then
   print_header "RUN cppcheck"
-  check_log "cppcheck $2 --enable=all --inconclusive --error-exitcode=1" "cppcheck failed"
+  check_log "cppcheck $2 --enable=all --inconclusive --error-exitcode=1 --suppress=missingIncludeSystem" "cppcheck failed"
 fi
 print_header "SUCCESS"
