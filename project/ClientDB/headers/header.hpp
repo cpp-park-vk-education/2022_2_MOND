b2b879cae15653e8a3f8fa93a3f9ce3578292a78
moraPresence <od34156la@mail.ru>
2022-11-19 14:20:10 +0300

    to_review

--- project/ClientDB/headers/header.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 MOND
-
-#ifndef MOND_DB_CLIENT_HEADER_H
-#define MOND_DB_CLIENT_HEADER_H
-
-int sum(int a, int b);
-
-#endif  // MOND_DB_CLIENT_HEADER_H
--- project/ServerDB/sources/source.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright 2022 MOND
-
-#include "header.hpp"
-int sum(int a, int b) { return a + b; }
