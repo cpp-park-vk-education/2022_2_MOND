7585778efbb334021db6707a4043ae4444b64619
ogrebatel <ogrebatelb@gmail.com>
2022-12-21 15:29:16 +0300

    performance testing

--- Tests/PerformanceClient/client.cpp
+++ Tests/PerformanceClient/client.cpp
@@ -6,7 +6,7 @@
 #include "ActionManager.h"
 
 
-void test_reading(int argc, char** argv){
+void test_reading(int argc, char** argv, int V, int N){
     for (int i = 0; i < argc; ++i) {
         std::cout << argv[i] << std::endl;
     }
@@ -16,7 +16,7 @@ void test_reading(int argc, char** argv){
     for (int i = 0; i < 10; ++i) {
         key.push_back(static_cast<char>(i));
     }
-    for (int i = 0; i < 5000; ++i) {
+    for (int i = 0; i < V; ++i) {
         value.push_back(static_cast<char>(i));
     }
     std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
@@ -29,9 +29,8 @@ void test_reading(int argc, char** argv){
     manager->SelectTable(argv[3]);
     manager->Insert(key, value);
 
-    for (int i = 0; i < 5000; ++i) {
+    for (int i = 0; i < N; ++i) {
         auto result = manager->Get(key);
-//        std::cout << result.size() << std::endl;
     }
     std::chrono::time_point<std::chrono::steady_clock> end = std::chrono::steady_clock::now();
     std::chrono::duration<unsigned long long, std::milli> int_ms =
@@ -42,17 +41,17 @@ void test_reading(int argc, char** argv){
     delete manager;
 }
 
-void test_writing(int argc, char** argv){
+void test_writing(int argc, char** argv, int V, int N){
     for (int i = 0; i < argc; ++i) {
         std::cout << argv[i] << std::endl;
     }
 
     std::string key;
     std::string value;
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 100; ++i) {
         key.push_back(static_cast<char>(i));
     }
-    for (int i = 0; i < 5000; ++i) {
+    for (int i = 0; i < V; ++i) {
         value.push_back(static_cast<char>(i));
     }
     std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
@@ -65,8 +64,8 @@ void test_writing(int argc, char** argv){
     manager->SelectTable(argv[3]);
     manager->Insert(key, value);
 
-    for (int i = 0; i < 5000; ++i) {
-        key.push_back(static_cast<char>(i));
+    for (int i = 0; i < N; ++i) {
+        key[i % 100] += static_cast<char>(i+i);
         auto result = manager->Insert(key, value);
     }
     std::chrono::time_point<std::chrono::steady_clock> end = std::chrono::steady_clock::now();
@@ -81,10 +80,13 @@ void test_writing(int argc, char** argv){
 
 int main(int argc, char** argv) {
 
+    int V = std::stoi(argv[5]);
+    int N = std::stoi(argv[6]);
+
     if (std::string(argv[4]) == "reading")
-        test_reading(argc, argv);
+        test_reading(argc, argv, V, N);
     else
-        test_writing(argc, argv);
+        test_writing(argc, argv, V, N);
 
     return 0;
 }
